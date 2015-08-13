e07bcf2192510d2fd759f298dbc9e1a26d2dba87
Mart95777 <marcin.m.niewiadomski@gmail.com>
2015-08-13 22:04:00 +0200

    view - splitPaneMain, buttons?

--- javaworkoutxviews/JavaWorkoutXView.java
+++ javaworkoutxviews/JavaWorkoutXView.java
@@ -6,23 +6,40 @@ package javaworkoutxviews;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
 /**
  * @author marcin
  *
  */
 public class JavaWorkoutXView extends JFrame {
+	/**
+	 * Fields
+	 */
+	JPanel mainPanel;
+	JSplitPane splitPaneMain;
 
 	/**
-	 * 
+	 * CONSTRUCTOR
 	 */
 	public JavaWorkoutXView() {
 		// TODO Auto-generated constructor stub
-		super();
-		this.setTitle("Java Workout X program, ...");
+		super("Java Workout X program, ...");
 		this.setPreferredSize(new Dimension(700,500));
 		// temporary
 		this.setSize(1600, 900);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		mainPanel = new JPanel();
+		
+		splitPaneMain = new JSplitPane();
+		mainPanel.add(splitPaneMain);
+		
+		
+		this.add(mainPanel);
+		this.pack();
 	}
 
 }
