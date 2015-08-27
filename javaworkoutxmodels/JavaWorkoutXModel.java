27529d7d9b3e352dcee89c0cc832d46276519ca2
Mart95777 <marcin.m.niewiadomski@gmail.com>
2015-08-27 22:56:36 +0200

    current user...2
    needs making modal dialog out of it.
    change OpenJWX

--- javaworkoutxmodels/JavaWorkoutXModel.java
+++ javaworkoutxmodels/JavaWorkoutXModel.java
@@ -3,9 +3,29 @@
  */
 package javaworkoutxmodels;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
+import javaworkoutx.JavaWorkoutX_old;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ListSelectionModel;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.xpath.XPathExpressionException;
 
 /**
  * @author marcin
@@ -13,7 +33,13 @@ import javax.swing.JOptionPane;
  */
 public class JavaWorkoutXModel {
 	
+	
+	File dataFolder = null;
 	File topicsFile = null;
+	File usersFolder = null;
+	File runningFolder = null;
+	
+	String currentUser = null;
 	
 
 	/**
@@ -21,11 +47,191 @@ public class JavaWorkoutXModel {
 	 */
 	public JavaWorkoutXModel() {
 		// TODO Auto-generated constructor stub
+		StringBuilder str1 = new StringBuilder();
+		str1.append(new File(".").getAbsolutePath());
+		runningFolder = new File(str1.toString()).getParentFile();
+		JOptionPane.showMessageDialog(null, "running folder: "+runningFolder);
+		currentUser = getCurrentUser();
+		JOptionPane.showMessageDialog(null, "current user: "+currentUser);
 	}
 	
 	public void readTopics(){
 		JOptionPane.showMessageDialog(null, "Inside readTopics method");
+		// getting current user
 		
+		
+	}
+	
+	public String getCurrentUser(){
+		OpenJWX frame = new OpenJWX(this.runningFolder);
+		return frame.currentUser;
 	}
 
 }
+
+class OpenJWX extends JFrame {
+	JPanel mainPanel;
+	JTextArea textUserSelection;
+	JTextArea labelNewUser;
+	JTextArea textNewUser;
+	JButton userCreate;
+	JButton userOK;
+	
+	File newUserFolder = null;
+	String currentUser;
+	
+	DefaultListModel<String> model;
+	JList<String> userList;
+	
+	
+	/**
+	 * Methods for the constructor - OpenJWX
+	 */
+	
+	private void addcomponent(JPanel pn, JComponent cmp, int xpos, int ypos, int w, int h, int place, int stretch){
+		GridBagConstraints gridcns = new GridBagConstraints();
+		//GridBagConstraints(int gridx, int gridy, int gridwidth, int gridheight, double weightx, 
+		// double weighty, int anchor, int fill, Insets insets, int ipadx, int ipady)
+		gridcns.gridx = xpos;
+		gridcns.gridy = ypos;
+		gridcns.gridwidth = w;
+		gridcns.gridheight = h;
+		gridcns.weightx = 100;
+		gridcns.weighty = 100;
+		gridcns.insets = new Insets(5,5,5,5);
+		gridcns.anchor = place;
+		gridcns.fill = stretch;
+		
+		pn.add(cmp, gridcns);
+	} // end private void addcomponent
+	
+	/**
+	 * CONSTRUCTOR!
+	 */
+	public OpenJWX(File runningFolder){
+		super("JAVA Workout - Selecting User");
+		this.setSize(450,200);
+		this.setLocationRelativeTo(null);
+		
+		boolean success;
+		File[] usersFolders = null;
+		
+		StringBuilder str2 = new StringBuilder();
+		//StringBuilder str1 = new StringBuilder();
+					
+		
+		
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new GridBagLayout());
+		
+		textUserSelection = new JTextArea();
+		//textUserSelection.setLineWrap(true);
+		//textUserSelection.setWrapStyleWord(true);
+		//textUserSelection.setText("Select user: (This will close this dialog box)");
+		textUserSelection.setText("Select user: ");
+		textUserSelection.setEditable(false);
+		textUserSelection.setOpaque(false);
+		addcomponent(mainPanel, textUserSelection, 0,0,1,1, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE);
+		
+		labelNewUser = new JTextArea();
+		labelNewUser.setText("New user: ");
+		labelNewUser.setEditable(false);
+		labelNewUser.setOpaque(false);
+		addcomponent(mainPanel, labelNewUser, 1,0,1,1, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE);
+		
+		textNewUser = new JTextArea();
+		textNewUser.setBackground(new Color(255,255,255));
+		textNewUser.setPreferredSize(new Dimension(150,17));
+		//textNewUser.setText("...");
+		textNewUser.setEditable(true);
+		textNewUser.setOpaque(true);
+		addcomponent(mainPanel, textNewUser, 1,1,1,1, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE);
+		
+		model = new DefaultListModel<>();
+		userList = new JList<>();
+		userList.setModel(model);
+		JScrollPane jscrl = new JScrollPane(userList);
+		Dimension dim = new Dimension(200,120);
+		jscrl.setPreferredSize(dim);
+		userList.setPreferredSize(dim);
+		//jscrl.setPreferredSize(new Dimension(200,120));
+		//test 15 elements
+//		for (int i = 0; i < 15; i++)
+//		      model.addElement("Element " + i);
+		// Finding all folders with users
+		str2.setLength(0);
+		str2.append(runningFolder.toString());
+		str2.append("\\data\\users\\");
+		
+		listFoldersOfUsers(usersFolders,model,str2.toString());
+		
+		userList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		addcomponent(mainPanel, jscrl, 0,1,1,6, GridBagConstraints.WEST, GridBagConstraints.NONE);
+		
+		userCreate = new JButton("Create user");
+		userCreate.addActionListener(new ActionListener() {
+			  public void actionPerformed(ActionEvent evt) {
+				  // 
+				  boolean success;
+				  
+				  str2.setLength(0);
+				  str2.append(runningFolder.toString());
+				  str2.append('\\');
+				  newUserFolder = new File(str2.toString(),"data\\users\\"+textNewUser.getText());
+				  success = newUserFolder.mkdir();
+				  JOptionPane.showMessageDialog(null, "success(new users folder created): "+success);
+				  str2.setLength(0);
+				  str2.append(runningFolder.toString());
+				  str2.append("\\data\\users\\");
+				  listFoldersOfUsers(usersFolders,model,str2.toString());
+			  }
+		});
+		addcomponent(mainPanel, userCreate, 1,2,1,1, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE);
+		
+		userOK = new JButton("OK, Start Program");
+		userOK.addActionListener(new ActionListener() {
+			  public void actionPerformed(ActionEvent evt) {
+				// 
+				currentUser = userList.getSelectedValue().toString();
+				//frame.textSelection.append(" "+frame.currentUser);
+				// dealing with user
+				//String s = currentUser;
+				// parsing xml
+				//DOMparseJWX parser1 = new DOMparseJWX((DocumentBuilder) frame.document, frame, runningFolder, s);
+				//dispose();
+			  }
+		});
+		addcomponent(mainPanel, userOK, 0,8,2,1, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE);
+		
+		
+		this.add(mainPanel);
+		this.pack();
+		this.setVisible(true);
+		// TESTING !!!!!!!!!!!!!!!!!!!!
+		//frame.appendTextSelection(" addition!");
+		
+	
+	}//end of Constructor OpenJWX
+	
+	/**
+	 * Other methods of OpenJWX
+	 */
+	private void listFoldersOfUsers(File[] usersFolders, DefaultListModel<String> model,String str){
+		StringBuilder str2 = new StringBuilder("");
+		model.clear();
+		try {
+			usersFolders = new File(str).listFiles();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			JOptionPane.showMessageDialog(null, "Problem with listing user Folders files");
+			e.printStackTrace();
+		}
+		for (File temp : usersFolders){
+			if(temp.isDirectory()){
+				str2.setLength(0);
+				str2.append(temp.getName().toString());
+				model.addElement(str2.toString());
+			}
+		}
+	}
+}// end of class OpenJWX
